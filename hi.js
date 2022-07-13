969b8a1919d8ce88adb55365abb147c6037ea850
666rubberducky <alexanderdoneski@gmail.com>
2022-07-14 00:08:13 +1000

    played with some more javascript stuff i have money changing in the background

--- hi.js
+++ hi.js
@@ -1,3 +1,5 @@
+
+
 function clicked(){
     const colours = ["red", "green", "blue", "yellow", "orange", "magenta", "pink", "SlateBlue", "DarkCyan", "GreenYellow", "Plum"]
     const hi = document.getElementById('button')
@@ -7,11 +9,19 @@ function clicked(){
 }
 
 
-function clipboardId() {
-    //select the element with the id "copyMe", must be a text box
-    var textToCopy = document.getElementById("copydiscord");
-    //select the text in the text box
-    textToCopy.select();
-    //copy the text to the clipboard
-    document.execCommand("copy");
-}
+setInterval(() => {
+    // const greens = ["#0B6623", "#708238", "#3F704D", "#8F9779", "#29AB87", "#00755E", "#3B7A57", "#004B49", "#1C352D", "#8A9A5B", "#39FF14", "#50C878", "#C7EA46"]
+    const colour = ["#d3abdd", "#e4a1f5", "#c487d3", "#df79f8", "#a850be", "#ca4fe9" ]
+    const money = document.getElementById('money')
+    const rand = Math.round(Math.random() * 10)
+    money.style.color = colour[rand]
+    // money.style.backgroundColor= colours[rand]
+}, 500) 
+
+// setInterval(() => {
+//     const pinks = ["#F0D3F7", "#d3abdd", "#e4a1f5", "#c487d3", "#df79f8", "#a850be", "#ca4fe9" ]
+//     const body = document.body
+//     const rand = Math.round(Math.random() * 10)
+//     body.style.backgroundColor = pinks[rand]
+//     // money.style.backgroundColor= colours[rand]
+// }, 10000)
